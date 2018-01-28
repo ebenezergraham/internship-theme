130741ead80caa4346f153a7ce2c0ce385d1f368
Ebenezer Graham <egraham15@alustudent.com>
2018-01-28 05:19:24 +0400

    s

--- assets/js/nippy.js
+++ assets/js/nippy.js
@@ -23,3 +23,29 @@ function showSlides(n) {
     }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";}
+
+  var outslideIndex = 1;
+  outshowSlides(outslideIndex);
+
+  function outplusSlides(n) {
+    outshowSlides(outslideIndex += n);
+  }
+
+  function outcurrentSlide(n) {
+    outshowSlides(outslideIndex = n);
+  }
+
+  function outshowSlides(n) {
+    var x;
+    var outslides = document.getElementsByClassName("outSlides");
+    var outdots = document.getElementsByClassName("dot");
+    if (n > outslides.length) {outslideIndex = 1}
+      if (n < 1) {outslideIndex = outslides.length}
+      for (x = 0; x < outslides.length; x++) {
+        outslides[x].style.display = "none";
+      }
+      for (x = 0; x < outdots.length; x++) {
+        outdots[x].className = outdots[x].className.replace(" active", "");
+      }
+    outslides[outslideIndex-1].style.display = "block";
+    outdots[outslideIndex-1].className += " active";}
